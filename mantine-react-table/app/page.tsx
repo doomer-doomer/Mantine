03bb6030e782b174d23439c4f1167dc6b76914e0
Tejas Gotavade <74721478+doomer-doomer@users.noreply.github.com>
2025-10-15 21:13:39 +0530

    done

--- mantine-react-table/app/page.tsx
+++ mantine-react-table/app/page.tsx
@@ -4,11 +4,6 @@ import '../css/home.css'
 
 export default function Home() {
 
-  function render(some:String): number{
-    console.log(some);
-    let numbers: number[] = [1,2,34,5]
-    return 0;
-  }
   return (
     <div className="main">
     <h1>My Work</h1>
